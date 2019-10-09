3a1f9ac1b8f8f621c19561da460c718be1e914f7
Sérgio Mafra <sergio@mafra.io>
2019-10-09 18:05:11 -0300

    Added email normalization to email validation

--- app/core/models.py
+++ app/core/models.py
@@ -3,17 +3,22 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
     PermissionsMixin
 
 
+# Class that provides helper functions to user's creation
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         ''' Creates and saves a new user '''
-        user = self.model(email=email, **extra_fields)
+
+        if not email:
+            raise ValueError('Users must have an email address')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
         return user
 
 
+# User model
 class User(AbstractBaseUser, PermissionsMixin):
     ''' Custom user model that supports using email instead of username '''
     email = models.EmailField(max_length=255, unique=True)
@@ -21,6 +26,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
+    # Assigning UserManager to objects
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -16,3 +16,17 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        ''' Test the email for a new user is normalized '''
+
+        email = 'test@MAFRA.IO'
+        user = get_user_model().objects.create_user(email, 'TestPass123')
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        ''' Test creating user with no email raises error '''
+
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'TestPass123')
